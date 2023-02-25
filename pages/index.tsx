6b77bbfbbc5318974e9c9f457075ffd2facd898c
godfgod1 <godfgod1@naver.com>
2023-02-26 00:50:55 +0900

    like 버튼 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,48 @@ import FloatingButton from '../components/floatingButton';
 export default () => (
   <div className='h-screen  '>
     {new Array(10).fill('test').map((e, i) =>
-    <Link href={`/tweet/${i}`}>
-      <div className='bg-green-100 w-full h-20  drop-shadow-lg px-5  border-b-8  border-gray-100  hover:bg-green-300 cursor-pointer '>
-        <div className='h-12 flex items-center px-3'>
-          <h1>{e}</h1>
-        </div>
-        <div className='h-8 flex items-center pb-3 w-full justify-between px-3'>
-          <div className='flex space-x-5'>
-            <span>
-              <svg className='   w-5' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"></path>
-              </svg>
-            </span>
-            <span>
-              {'1'}
-            </span>
+      <Link href={`/tweet/${i}`}>
+        <div className='bg-green-100 w-full h-20  drop-shadow-lg px-5  border-b-8  border-gray-100  hover:bg-green-300 cursor-pointer '>
+          <div className='h-12 flex items-center px-3'>
+            <h1>{e}</h1>
           </div>
-          <div>
-            by asd
+          <div className='h-8 flex items-center pb-3 w-full justify-between px-3'>
+            <div className='flex space-x-5'>
+
+              <div className='flex space-x-2'>
+                <svg className='   w-5' fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"></path>
+                </svg>
+                <span>{'1'}</span>
+              </div>
+              <div className='flex space-x-2'>
+                <span >
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </span>
+                <span>{'1'}</span>
+              </div>
+            </div>
+            <div>
+              by asd
+            </div>
           </div>
         </div>
-      </div>
       </Link>
     )}
 
- <FloatingButton href="/write"/>
-    
-    
+    <FloatingButton href="/write" />
+
+
   </div>
 );
--- pages/tweet/[id].tsx
+++ pages/tweet/[id].tsx
@@ -32,7 +32,39 @@ export default () => {
       <div className='h-[20rem]  border border-green-300 p-10 flex flex-col   justify-between'>
 
       </div>
-      <form onSubmit={handleSubmit(onValid)} className='h-[12rem] w-full  border border-green-300 p-5 flex flex-col justify-between '>
+      <form onSubmit={handleSubmit(onValid)} className='h-[14rem] w-full  border border-green-300 p-5 flex flex-col justify-between '>
+        <div className='w-full flex justify-end  space-x-2'>
+      {/* <svg
+     className="w-6 h-6"
+     fill="currentColor"
+     viewBox="0 0 20 20"
+     xmlns="http://www.w3.org/2000/svg"
+   >
+     <path
+       fillRule="evenodd"
+       d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+       clipRule="evenodd"
+     ></path>
+   </svg> */}
+   <span>Like </span>
+   <button onClick={()=>alert('Like')}>
+<svg
+className="h-6 w-6 "
+xmlns="http://www.w3.org/2000/svg"
+fill="none"
+viewBox="0 0 24 24"
+stroke="currentColor"
+aria-hidden="true"
+>
+<path
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  strokeWidth="2"
+  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+/>
+</svg>
+</button>
+        </div>
         <TextArea register={register("write", { required: true })} name={'댓글'} />
         <Button text={"등록"} />
       </form>
